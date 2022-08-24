0e441a8b75b48da0bd3ccc4c8ea69dc8968fd3ab
Josh Mo <joshua.om.876@gmail.com>
2022-08-24 21:53:43 +0100

    typo fix v2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React , {useContext} from 'react'
 import {Link} from 'react-router-dom'
-import { Square } from '../assets/square';
+import { Square } from '../assets/Square';
 import { login } from '../login';
 import { userContext } from '../userContext';
 
